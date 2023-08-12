fb73ae73b9fc6b7795385f9e27126689db18ea69
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-12 10:35:10 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="cockpit"
+#REMOVEME? pkg_dependencies="cockpit"
 
 #=================================================
 # PERSONAL HELPERS
